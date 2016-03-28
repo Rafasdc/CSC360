e6ee4ab944530a1915c6a581c6e0b9d8fe036a43
Rafael Solorzano <rafa@solorzanoc.com>
2016-03-28 02:56:59 -0700

    Part2

--- A3/disklist.cpp
+++ A3/disklist.cpp
@@ -11,7 +11,48 @@
 #include <stdlib.h>
 #include <string>
 
+int getRootDirStart(FILE *fp){
+	int high;
+	int low;
+	unsigned char high_temp;
+	unsigned char low_temp;
+	int retVal;
+	fseek(fp,ROOTDIRSTART_OFFSET,SEEK_SET);
+	fread(&high_temp,1,1,fp);
+	fread(&low_temp,1,1,fp);
+	high = ((high_temp<<8)) + low_temp;
+	fseek(fp,ROOTDIRSTART_OFFSET+2,SEEK_SET);
+	fread(&high_temp,1,1,fp);
+	fread(&low_temp,1,1,fp);
+	low = ((high_temp<<8)) + low_temp;
+	retVal = ((high<<8)) + low;
+	return retVal;
+};
 
+int getRootDirBlocks(FILE *fp){
+	int high;
+	int low;
+	unsigned char high_temp;
+	unsigned char low_temp;
+	int retVal;
+	fseek(fp,ROOTDIRBLOCKS_OFFSET,SEEK_SET);
+	fread(&high_temp,1,1,fp);
+	fread(&low_temp,1,1,fp);
+	high = ((high_temp<<8)) + low_temp;
+	fseek(fp,ROOTDIRBLOCKS_OFFSET+2,SEEK_SET);
+	fread(&high_temp,1,1,fp);
+	fread(&low_temp,1,1,fp);
+	low = ((high_temp<<8)) + low_temp;
+	retVal = ((high<<8)) + low;
+	return retVal;
+};
+
+void getFile(FILE *fp){
+	unsigned char type;
+	//if 03 is file if 05 is directory becaus eof bits set in binary
+	fread(&type,1,1,fp);
+	printf("%d\n",type);
+}
 
 int main(int argc, char** argv)
 {
@@ -19,11 +60,11 @@ int main(int argc, char** argv)
 	FILE *fp;
 	if ((fp=fopen(argv[1],"r")))
 	{
-
+		fseek(fp, getRootDirStart(fp)*512,SEEK_SET);
+		getFile(fp);
 
 	} else
 		printf("Fail to open the image file.\n");
-	free(name);
 	fclose(fp);
 	return 0;
 }
