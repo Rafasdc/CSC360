12b9a7483b2362f4bf3c92499a4b4ea6a02d4623
Rafasdc <rafa@solorzanoc.com>
2016-03-28 04:09:03 -0700

    Part2 Done
    Still missing part1 to replace constants

--- A3/disklist.cpp
+++ A3/disklist.cpp
@@ -11,6 +11,8 @@
 #include <stdlib.h>
 #include <string>
 
+int currentFile;
+
 int getRootDirStart(FILE *fp){
 	int high;
 	int low;
@@ -47,22 +49,86 @@ int getRootDirBlocks(FILE *fp){
 	return retVal;
 };
 
-void getFile(FILE *fp){
+int getFileType(FILE *fp){
 	unsigned char type;
 	//if 03 is file if 05 is directory becaus eof bits set in binary
 	fread(&type,1,1,fp);
-	printf("%d\n",type);
+	//printf("%d\n",type);
+	return type;
+}
+int getSize(FILE *fp){
+	int high;
+	int low;
+	unsigned char high_temp;
+	unsigned char low_temp;
+	int retVal;
+	fseek(fp,DIRECTORY_FILE_SIZE_OFFSET-1,SEEK_CUR);
+	fread(&high_temp,1,1,fp);
+	fread(&low_temp,1,1,fp);
+	high = ((high_temp<<8)) + low_temp;
+	//printf("%d\n",high);
+	fread(&high_temp,1,1,fp);
+	fread(&low_temp,1,1,fp);
+	low = ((high_temp<<8)) + low_temp;
+	//printf("%d\n",low);
+	retVal = ((high<<8)) + low;
+	return retVal;
+}
+
+void getName (FILE *fp){
+	char name[DIRECTORY_MAX_NAME_LENGTH];
+	fseek(fp,DIRECTORY_FILENAME_OFFSET+currentFile,SEEK_SET);
+	fread(&name,1,31,fp);
+	printf("%*s ",DIRECTORY_MAX_NAME_LENGTH,name);
+}
+
+void getModified (FILE *fp){
+	fseek(fp,DIRECTORY_MODIFY_OFFSET+currentFile,SEEK_SET);
+	unsigned char high;
+	unsigned char low;
+	int YYYY;
+	int MM;
+	int DD;
+	int HH;
+	int mm;
+	int SS;
+	fread(&high,1,1,fp);
+	fread(&low,1,1,fp);
+	YYYY = ((high)<<8) + low;
+	fread(&MM,1,1,fp);
+	fread(&DD,1,1,fp);
+	fread(&HH,1,1,fp);
+	fread(&mm,1,1,fp);
+	fread(&SS,1,1,fp);
+	printf("%d/%02d/%02d %02d:%02d:%02d\n",YYYY, MM, DD, HH, mm, SS);
 }
 
 int main(int argc, char** argv)
 {
 
+	int filetype = 1;
+	int size;
 	FILE *fp;
+
 	if ((fp=fopen(argv[1],"r")))
 	{
-		fseek(fp, getRootDirStart(fp)*512,SEEK_SET);
-		getFile(fp);
-
+		currentFile = getRootDirStart(fp)*512;
+		while (1){
+			fseek(fp, currentFile,SEEK_SET);
+			filetype = getFileType(fp);
+			if (filetype == 3){
+				printf("F ");
+			} else if (filetype == 5){
+				printf("D ");
+			} else {
+				break;
+			}
+			size = getSize(fp);
+			printf("%*d ",10,size);
+			getName(fp);
+			getModified(fp);
+			currentFile+=64;
+		}
 	} else
 		printf("Fail to open the image file.\n");
 	fclose(fp);
